bd7b90e4b492c66db138a1f8291d0177cc06669f
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-15 21:30:47 +0200

    changed end points for comments

--- src/index.ts
+++ src/index.ts
@@ -19,42 +19,53 @@ app.get('/', (req, res) => {
 })
 
 app.get('/posts', async (req, res) => {
-    const posts = await prisma.post.findMany({ include: { author: true, likes: true, comments:{include: {user:true}} } })
+    const posts = await prisma.post.findMany({ include: { author: true, likes: true, comments: { include: { user: true } } } })
     res.send(posts)
 })
 
-app.get('/posts/:id', async (req, res)=>{
-    const id=Number(req.params.id)
-    const singlePost= await prisma.post.findUnique({where: {id:id}, include: {author:true, likes:true, comments: {include: {user:true} }}})
-    if(singlePost){
+app.get('/posts/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const singlePost = await prisma.post.findUnique({ where: { id: id }, include: { author: true, likes: true, comments: { include: { user: true } } } })
+    if (singlePost) {
         res.send(singlePost)
     }
-    else{
-        res.status(404).send({error: "Post not found."})
+    else {
+        res.status(404).send({ error: "Post not found." })
     }
 })
 
-app.get('/users', async (req, res)=>{
-    const users = await prisma.user.findMany({include: {posts:true}})
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany({ include: { posts: true } })
     res.send(users)
 })
 
-app.post('/posts', async (req, res)=>{
-    await prisma.post.create({data: req.body})
-    const posts = await prisma.post.findMany({ include: { author: true, likes: true, comments:{include: {user:true}} } })
+app.get('/users/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const user = await prisma.user.findUnique({ where: { id: id }, include: { posts: true } })
+    if (user) {
+        res.send(user)
+    }
+    else {
+        res.status(404).send({ error: "User not found!" })
+    }
+})
+
+app.post('/posts', async (req, res) => {
+    await prisma.post.create({ data: req.body })
+    const posts = await prisma.post.findMany({ include: { author: true, likes: true, comments: { include: { user: true } } } })
     res.send(posts)
 })
 
-app.post('/likes', async (req, res)=>{
-    await prisma.likes.create({data: req.body})
-    const posts = await prisma.post.findMany({ include: { author: true, likes: true, comments:{include: {user:true}} } })
+app.post('/likes', async (req, res) => {
+    await prisma.likes.create({ data: req.body })
+    const posts = await prisma.post.findMany({ include: { author: true, likes: true, comments: { include: { user: true } } } })
     res.send(posts)
 })
 
-app.post('/comments', async (req, res)=>{
-    await prisma.comments.create({data:req.body, include:{user:true}})
-    const posts = await prisma.post.findMany({ include: { author: true, likes: true, comments:{include: {user:true}} } })
-    res.send(posts)
+app.post('/comments', async (req, res) => {
+    const newComment=await prisma.comments.create({ data: req.body, include: { user: true } })
+    const singlePost = await prisma.post.findUnique({ where: { id: newComment.postId }, include: { author: true, likes: true, comments: { include: { user: true } } } })
+    res.send(singlePost)
 })
 
 app.listen(port)
